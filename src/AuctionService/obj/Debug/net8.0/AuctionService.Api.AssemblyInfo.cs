8991fba2b84db621fb97ab4fb789ddf26450c65f
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2025-01-13 22:45:28 +0200

    Fixing migrations

--- src/AuctionService/obj/Debug/net8.0/AuctionService.Api.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298a15cb8e35aca33d3ede8c90c3cc6d20f253e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
